6fafc6e82a6a170a0a6d2be87b84f275718d974e
Code Monkey <codemonkey@bananolotto.com>
2021-03-02 05:38:37 +0000

    add wallet_api

---
+++ wallet_api.py
@@ -0,0 +1,40 @@
+import requests
+import settings
+import uuid
+import json
+
+def get_balance(account):
+    d = {
+        "action": "account_balance",
+        "account": account
+    }
+    data = json.dumps(d)
+    return requests.post(settings.wallet_api, data)
+
+def receive_pending(account):
+    d = {
+        "action": "receive_all",
+        "wallet": settings.wallet_id
+    }
+    data = json.dumps(d)
+    return requests.post(settings.wallet_api, data)
+
+def get_transactions(account, count):
+    d = {
+        "action": "account_history",
+        "account": account,
+        "count": count
+    }
+    return requests.get(settings.public_rpc, d)
+
+def send_banano(address, amount):
+    d = {
+        "action": "send",
+        "wallet": settings.wallet_id,
+        "source": settings.ban_account,
+        "destination": address,
+        "amount": amount,
+        "id": str(uuid.uuid1())
+    }
+    data = json.dumps(d)
+    return requests.post(settings.wallet_api, data)
